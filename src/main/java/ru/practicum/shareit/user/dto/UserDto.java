c0027f9d448e24535af4bb3eab615433f822d68b
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-13 21:33:54 +0300

    feat: добавить dto для сущности user
    Создать DTO-класс, класс который содержит необходимую для передачи информацию о сущности "пользователь"

--- src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -1,4 +1,31 @@
 package ru.practicum.shareit.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * <p>Data Transfer Object для сущности "Пользователь"</p>
+ * <p>Dto-класс User содержит поля:</p>
+ * <ul>
+ *     <li>id - уникальный идентификатор пользователя, тип Long;</li>
+ *     <li>name - имя пользователя, тип String;</li>
+ *     <li>email - адрес электронной почты пользователя, тип String.</li>
+ * </ul>
+ */
+
+@Data
+@Builder
+@AllArgsConstructor
 public class UserDto {
+    @Min(value = 1L, message = "Идентификатор должен быть положительным числом")
+    private Long id;
+    private String name;
+    @NotBlank
+    @Email(message = "Некорректный email")
+    private String email;
 }
