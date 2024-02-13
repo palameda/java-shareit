2f8214f73e0ca705189a73543775a75eeebc8b0b
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-13 21:30:16 +0300

    refactor: изменить структуру папок проекта

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit.item.model;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import ru.practicum.shareit.user.User;
+import ru.practicum.shareit.user.model.User;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
