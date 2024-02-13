aba520edcde8ec8fc3dc463b23e464bdf21f99dd
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-13 20:59:11 +0300

    feat: добавить dto для сущности item
    Создать DTO-класс, который содержит необходимую для передачи информацию о сущности "вещь"

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,33 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 /**
- * TODO Sprint add-controllers.
+ * <p>Data Transfer Object для сущности "Вещь"</p>
+ * <p>Data-класс Item содержит поля:</p>
+ * <ul>
+ *     <li>id - уникальный идентификатор вещи, тип Long;</li>
+ *     <li>name - краткое название, тип String;</li>
+ *     <li>description - развёрнутое описание, тип String;</li> *
+ *     <li>available - статус о доступности вещи для аренды, тип Boolean.</li>
+ * </ul>
  */
+
+@Data
+@Builder
+@AllArgsConstructor
 public class ItemDto {
+    @Min(value = 1L, message = "Идентификатор должен быть положительным числом")
+    private Long id;
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+    @Size(max = 255, message = "Размер описания не должен превышать 255 символов")
+    private String description;
+    private Boolean available;
 }
