78402ff08fa0cdeb6cea1de0241ff15e41c3e776
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-14 18:11:20 +0300

    feat: добавить интерфейс ItemRepository
    Интерфейс ItemRepository содежит сигнатуры метотов, опреледяющих функционал, который будет реальзован в классах, описывающих слой хранилища для сущности Item

--- src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/ItemRepository.java
@@ -1,2 +1,51 @@
-package ru.practicum.shareit.item.repository;public interface ItemRepository {
+package ru.practicum.shareit.item.repository;
+
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+
+/**
+ * Интерфейс ItemRepository содежит сигнатуры метотов, опреледяющих функционал,
+ * который будет реальзован в классах, описывающих слой хранилища для сущности Item
+ */
+public interface ItemRepository {
+    /**
+     * Метод getAllItems возвращает список зарегистрированных вещей
+     * @return List
+     */
+    List<Item> getAllItems();
+
+    /**
+     * Метод getById возвращает данные о зарегистрированной вещи по переданному идентификатору
+     * @param id
+     * @return Item
+     */
+    Item getById(Integer id);
+
+    /**
+     * Метод addItem добавляет в хранилище данные о новой вещи
+     * @param item
+     * @return Item
+     */
+    Item addItem(Item item);
+
+    /**
+     * Метод updateItem обновляет данные о вещи в хранилище
+     * @param item
+     * @return Item
+     */
+    Item updateItem(Item item);
+
+    /**
+     * Метод deleteItem удаляет данные о вещи в хранилище
+     * @param id
+     */
+    void deleteItem(Integer id);
+
+    /**
+     * Метод seekItem возвращает список вещей, найденных по запросу searchQuery
+     * @param searchQuery
+     * @return List
+     */
+    List<Item> seekItem(String searchQuery);
 }
