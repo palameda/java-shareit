04ac4da45d3b5894b44a8a97d13d81f35257e4a8
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-14 17:38:14 +0300

    feat: добавить интерфейс UserRepository
    Интерфейс UserRepository содежит сигнатуры метотов, опреледяющих функционал, который будет реальзован в классах, описывающих слой хранилища

--- src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/repository/UserRepository.java
@@ -1,4 +1,48 @@
 package ru.practicum.shareit.user.repository;
 
+import ru.practicum.shareit.user.model.User;
+
+import java.util.List;
+
+/**
+ * Интерфейс UserRepository содежит сигнатуры метотов, опреледяющих функционал,
+ * который будет реальзован в классах, описывающих слой хранилища
+ */
+
 public interface UserRepository {
+    /**
+     * Метод getAllUsers возвращает список зарегистрированных пользователей
+     * @return List
+     */
+    List<User> getAllUsers();
+
+    /**
+     * Метод getById возвращает данные зарегистрированного пользователя по переданному идентификатору
+     * @param id
+     * @return User
+     */
+    User getById(Long id);
+
+
+    /**
+     * Метод addUser добавляет в хранилище нового пользователя
+     * @param user
+     * @return User
+     */
+    User addUser(User user);
+
+    /**
+     * Метод updateUser обновляет данные зарегистрированного пользователя
+     * @param user
+     * @return User
+     */
+    User updateUser(User user);
+
+
+    /**
+     * Метод deleteUser удаляет из хранилища данные зарегистрированного пользователя по переданному идентификатору
+     * @param id
+     */
+    void deleteUser(Long id);
+
 }
