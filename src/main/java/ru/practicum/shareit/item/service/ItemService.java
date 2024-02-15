9f20e6bcb8474948debd044d33665b11f6086d90
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-15 16:30:53 +0300

    feat: добавить интерфейс ItemService
    Интерфейс ItemService содежит сигнатуры метотов, опреледяющих функционал, который будет реальзован в классах, описывающих слой сервиса для сущности Item.

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -1,4 +1,59 @@
 package ru.practicum.shareit.item.service;
 
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.exception.DenialOfAccessException;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import java.util.List;
+
+/**
+ * Интерфейс ItemService содежит сигнатуры метотов, опреледяющих функционал,
+ * который будет реальзован в классах, описывающих слой сервиса для сущности {@link Item}.
+ */
 public interface ItemService {
+    /**
+     * Сервисный метод findAll возвращает список вещей, зарегистрированных на пользователя по userId.
+     * @param  userId идентификатор владельца
+     * @return список всех вещей владельца, преобразованных в ItemDto
+     */
+    List<ItemDto> findAll(Integer userId);
+
+    /**
+     * Сервисный метод findById возвращает данные о зарегистрированной вещи по переданному идентификатору.
+     * @param itemId идентификатор вещи
+     * @return преобразованный в ItemDto объект класса Item
+     */
+    ItemDto findById(Integer itemId);
+
+    /**
+     * Сервисный метод saveItem отправляет запрос к хранилищу на сохранение данных о новой вещи её владельцем.
+     * @param itemDto объект класса ItemDto
+     * @param userId идентификатор владельца вещи
+     * @return преобразованный в ItemDto объект класса Item
+     */
+    ItemDto saveItem(ItemDto itemDto, Integer userId);
+
+    /**
+     * Сервисный метод updateItem отправляет запрос к хранилищу на обновление данных о вещи её владельцем
+     * @param itemDto объект класса ItemDto
+     * @param userId идентификатор владельца вещи
+     * @return преобразованный в ItemDto объект класса Item
+     */
+    ItemDto updateItem(ItemDto itemDto, Integer userId);
+
+    /**
+     * Сервисный метод deleteItem отправляет запрос к хранилищу на удаление данных о вещи её владельцем.
+     * При попытке удаления вещи пользователем, не являющимся её владельцем,
+     * возникает исключение {@link DenialOfAccessException}
+     * @param itemId идентификатор вещи
+     * @param userId идентификатор владельца вещи
+     */
+    void deleteItem(Integer itemId, Integer userId);
+
+    /**
+     * Сервисный метод seekItem возвращает список вещей, найденных по запросу searchQuery
+     * @param searchQuery поисковый запрос, содержащий имя или описание вещи
+     * @return список вещей, преобразованных в ItemDto, которые были найдены по поисковому запросу
+     */
+    List<ItemDto> seekItem(String searchQuery);
 }
