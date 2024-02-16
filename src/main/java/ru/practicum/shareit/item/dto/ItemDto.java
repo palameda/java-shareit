8f444280353211eb2b78a3709cb76683fe72a9de
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-17 00:11:08 +0300

    fix: скрыть данные владельца вещи при передаче данных

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,6 +1,5 @@
 package ru.practicum.shareit.item.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
@@ -16,23 +15,20 @@ import javax.validation.constraints.Size;
  *     <li>id - уникальный идентификатор вещи, тип Integer;</li>
  *     <li>name - краткое название, тип String;</li>
  *     <li>description - развёрнутое описание, тип String;</li>
- *     <li>ownerId - уникальный идентификатор владельца вещи, тип Integer</li>
  *     <li>available - статус о доступности вещи для аренды, тип Boolean.</li>
  * </ul>
  */
 
 @Data
 @Builder
-@AllArgsConstructor
 public class ItemDto {
     @Min(value = 1, message = "Идентификатор должен быть положительным числом")
     private Integer id;
     @NotBlank(message = "Название не может быть пустым")
     private String name;
+    @NotBlank(message = "Описание не может быть пустым")
     @Size(max = 255, message = "Размер описания не должен превышать 255 символов")
     private String description;
-    @NotNull(message = "У вещи должен быть владелец")
-    @Min(value = 1, message = "Идентификатор должен быть положительным числом")
-    private Integer ownerId;
+    @NotNull(message = "Не указан статус для аренды")
     private Boolean available;
 }
