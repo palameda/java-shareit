9b21299f09b401cabed0b1d88a63d37e6f5ee520
palameda <stanislav.s.mazurenko@yandex.ru>
2024-02-17 00:13:23 +0300

    fix: исправить логику работы метода saveItem(ItemDto, Integer)

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImplementation.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImplementation.java
@@ -5,10 +5,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import ru.practicum.shareit.exception.DenialOfAccessException;
 import ru.practicum.shareit.item.dto.ItemDto;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.item.utility.ItemMapper;
 import ru.practicum.shareit.user.repository.UserRepository;
-import ru.practicum.shareit.item.model.Item;
 
 import java.util.List;
 import java.util.Objects;
@@ -47,9 +47,10 @@ public class ItemServiceImplementation implements ItemService {
     public ItemDto saveItem(ItemDto itemDto, Integer userId) {
         log.info("Сервис: обработка запроса на сохранение вещи {} её владельцем с id {}", itemDto.getName(), userId);
         userRepository.getById(userId);
-        itemDto.setOwnerId(userId);
+        Item savedItem = ItemMapper.dtoToItem(itemDto);
+        savedItem.setOwnerId(userId);
         return ItemMapper.itemToDto(
-                itemRepository.saveItem(ItemMapper.dtoToItem(itemDto))
+                itemRepository.saveItem(savedItem)
         );
     }
 
@@ -88,7 +89,8 @@ public class ItemServiceImplementation implements ItemService {
      */
     private void checkPossession(ItemDto itemDto, Integer ownerId) {
         log.info("Сервис: проверка принадлежности вещи {} пользователю с id {}", itemDto.getName(), ownerId);
-        if (!Objects.equals(itemDto.getOwnerId(), ownerId)) {
+        Item savedItem = itemRepository.getById(itemDto.getId());
+        if (!Objects.equals(savedItem.getOwnerId(), ownerId)) {
             throw new DenialOfAccessException(
                     "Отказ в доступе. Пользователь с id " + ownerId + " не является владельцем вещи " + itemDto.getName()
             );
