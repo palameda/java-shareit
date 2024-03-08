80fcede4142d1a9bd9d0f9ef4ed0db1c9a1c10b8
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-08 16:46:44 +0300

    refactor: добавить конструкторы в класс

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,7 +1,9 @@
 package ru.practicum.shareit.user.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
@@ -15,6 +17,8 @@ import javax.persistence.*;
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
---
+++ src/main/java/ru/practicum/shareit/user/repository/db/UserDbRepository.java
@@ -0,0 +1,4 @@
+package ru.practicum.shareit.user.repository.db;
+
+public class UserDbRepository {
+}
