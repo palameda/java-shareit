c1321b21ed5fcdcb8d35e114711c312c98d664f7
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-15 20:22:42 +0300

    style: удалить лишний импорт

--- src/main/java/ru/practicum/shareit/exception/NotFoundException.java
+++ src/main/java/ru/practicum/shareit/exception/NotFoundException.java
@@ -1,12 +1,9 @@
 package ru.practicum.shareit.exception;
 
-import lombok.extern.slf4j.Slf4j;
-
 /**
  * Исключение NotFoundException возникает при запросе данных, которые отсутствуют в хранилище
  */
 
-@Slf4j
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String message) {
         super(message);
