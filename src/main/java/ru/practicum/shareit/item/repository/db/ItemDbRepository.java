b51a95b9d48961f13369b1fa828956e544af8471
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-15 20:16:03 +0300

    feat: добавить jpa репозиторий для работы с item

--- src/main/java/ru/practicum/shareit/item/repository/db/ItemDbRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/db/ItemDbRepository.java
@@ -1,4 +1,15 @@
 package ru.practicum.shareit.item.repository.db;
 
-public interface ItemDbRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
+
+public interface ItemDbRepository extends JpaRepository<Item, Integer> {
+    List<Item> findAllByOwnerId(Integer ownerId);
+
+    @Query(value = "select it.id, it.name, it.description, it.available, it.owner_id as owner from items as it " +
+            "where ((it.name ilike %?1%) or (it.description ilike %?1%)) and it.available", nativeQuery = true)
+    List<Item> findByNameOrDescriptionAndAvailable(String query);
 }
