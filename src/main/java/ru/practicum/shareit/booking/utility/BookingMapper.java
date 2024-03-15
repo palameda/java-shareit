3146091dec4cc223277f0ee4a864b8fddd5232f8
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-15 20:27:09 +0300

    feat: добавить mapper для работы с моделью и dto сущности "бронирование"

--- src/main/java/ru/practicum/shareit/booking/utility/BookingMapper.java
+++ src/main/java/ru/practicum/shareit/booking/utility/BookingMapper.java
@@ -1,4 +1,41 @@
 package ru.practicum.shareit.booking.utility;
 
+import ru.practicum.shareit.booking.dto.BookingDto;
+import ru.practicum.shareit.booking.dto.DatelessBookingDto;
+import ru.practicum.shareit.booking.model.Booking;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
 public class BookingMapper {
+
+    public static BookingDto bookingToDto(Booking booking) {
+        return BookingDto.builder()
+                .id(booking.getId())
+                .itemId(booking.getItem().getId())
+                .userId(booking.getBooker().getId())
+                .start(booking.getStart())
+                .end(booking.getEnd())
+                .status(booking.getStatus())
+                .build();
+    }
+
+    public static Booking dtoToBooking(BookingDto bookingDto, Item item, User booker) {
+        return Booking.builder()
+                .id(bookingDto.getId())
+                .item(item)
+                .booker(booker)
+                .start(bookingDto.getStart())
+                .end(bookingDto.getEnd())
+                .status(bookingDto.getStatus())
+                .build();
+    }
+
+    public static DatelessBookingDto bookingToDateless(Booking booking) {
+        return DatelessBookingDto.builder()
+                .bookingId(booking.getId())
+                .item(booking.getItem())
+                .booker(booking.getBooker())
+                .status(booking.getStatus())
+                .build();
+    }
 }
