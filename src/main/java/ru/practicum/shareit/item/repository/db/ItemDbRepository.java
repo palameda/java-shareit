bb52db7db5059925c3965cc2629fa9a80b90c6db
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-16 09:09:46 +0300

    fix: исправить запрос

--- src/main/java/ru/practicum/shareit/item/repository/db/ItemDbRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/db/ItemDbRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 public interface ItemDbRepository extends JpaRepository<Item, Integer> {
     List<Item> findAllByOwnerId(Integer ownerId);
 
-    @Query(value = "select it.id, it.name, it.description, it.available, it.owner_id as owner from items as it " +
+    @Query(value = "select it.item_id, it.name, it.description, it.available, it.owner_id as owner from items as it " +
             "where ((it.name ilike %?1%) or (it.description ilike %?1%)) and it.available", nativeQuery = true)
     List<Item> findByNameOrDescriptionAndAvailable(String query);
 }
