c74d7f7b806981380f0290d9f824f1aae21cebb3
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-18 21:07:55 +0300

    feat: добавить интерфейс для репозитория RequestDbRepository

--- src/main/java/ru/practicum/shareit/request/repository/db/RequestDbRepository.java
+++ src/main/java/ru/practicum/shareit/request/repository/db/RequestDbRepository.java
@@ -1,4 +1,18 @@
 package ru.practicum.shareit.request.repository.db;
 
-public class RequestDbRepository {
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import ru.practicum.shareit.request.model.ItemRequest;
+
+import java.util.List;
+
+/**
+ * Интерфейс RequestDbRepository содержит сигнатуры методов для работы с сущностью {@link ItemRequest}.
+ * Расширяет {@link PagingAndSortingRepository}, предоставляющий методы для получения сущностей
+ * с использованием абстракции пагинации и сортировки
+ */
+public interface RequestDbRepository extends PagingAndSortingRepository<ItemRequest, Integer> {
+    List<ItemRequest> findAllByAuthorIdOrderByIdDesc(Integer userId);
+
+    List<ItemRequest> findAllByAuthorIdNot(Integer userId, Pageable pageable);
 }
