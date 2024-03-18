de1b52cc83daae231d4addf622b8415a714ce41b
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-18 20:06:49 +0300

    feat: добавить таблицу Requests в schema.sql

---
+++ src/main/java/ru/practicum/shareit/request/repository/db/RequestDbRepository.java
@@ -0,0 +1,4 @@
+package ru.practicum.shareit.request.repository.db;
+
+public class RequestDbRepository {
+}
---
+++ src/main/java/ru/practicum/shareit/request/utility/RequestMapper.java
@@ -0,0 +1,4 @@
+package ru.practicum.shareit.request.utility;
+
+public class RequestMapper {
+}
