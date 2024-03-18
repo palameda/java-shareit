2d19a7dd92227ce10fc61328e1d2f9c26c290a43
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-18 21:30:14 +0300

    feat: добавить интерфейс для сервиса ItemRequestService

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestService.java
@@ -1,4 +1,53 @@
 package ru.practicum.shareit.request.service;
 
-public interface RequestService {
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.dto.ItemResponseDto;
+import ru.practicum.shareit.request.model.ItemRequest;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+/**
+ * Интерфейс ItemRequestService содежит сигнатуры метотов, опреледяющих функционал,
+ * который будет реальзован в классах, описывающих слой сервиса для сущности {@link ItemRequest}.
+ */
+public interface ItemRequestService {
+
+    /**
+     * Метод для получения запроса на создание вещи по его идентификатору
+     * @param itemRequestId идентификатор запроса на создание вещи
+     * @param userId идентификатор пользователя, осуществляющего запрос
+     * @return объект класса {@link ItemRequest}
+     */
+    ItemRequest findRequestById(Integer itemRequestId, Integer userId);
+
+    /**
+     * Метод для получения запроса на создание вещи по его идентификатору
+     * @param itemRequestId идентификатор запроса на создание вещи
+     * @param userId идентификатор пользователя, осуществляющего запрос
+     * @return объект класса {@link ItemResponseDto}
+     */
+    ItemResponseDto findById(Integer itemRequestId, Integer userId);
+
+    /**
+     * Метод для получения всех запросов для их автора
+     * @param userId идентификатор автора запроса
+     * @return список объектов класса {@link ItemResponseDto}
+     */
+    List<ItemResponseDto> findAllByAuthor(Integer userId);
+
+    /**
+     * Метод для получения всех запросов пользователя
+     * @param userId идентификатор пользователя
+     * @param pageable критерий отображения результатов
+     * @return список объектов класса {@link ItemResponseDto}
+     */
+    List<ItemResponseDto> findAll(Integer userId, Pageable pageable);
+
+    /**
+     * Метод для сохранения запроса на добавление вещи
+     * @param requestDto объект класса {@link ItemRequestDto}
+     * @return объект класса {@link ItemResponseDto}
+     */
+    ItemResponseDto save(ItemRequestDto requestDto);
 }
