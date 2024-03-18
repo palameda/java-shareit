482f93080ac105d32064fba6a664210cc8590dec
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-18 20:32:10 +0300

    feat: добавить dto-класс ItemRequestDto (формат запроса)

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,33 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
 /**
- * TODO Sprint add-item-requests.
+ * Data Transfer Object для сущности "Запрос на добавление вещи".
+ * Dto-класс ItemRequestDto содержит поля:
+ * <ul>
+ *     <li>userId - идентификатор пользователя, оставившего запрос, тип {@link Integer}</li>
+ *     <li>description - текстовое описание запроса, тип {@link String}</li>
+ *     <li>created - дата и время создания запроса, тип {@link LocalDateTime}</li>
+ * </ul>
  */
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class ItemRequestDto {
+    private Integer userId;
+    @NotBlank(message = "Описание запроса на добаление вещи не может быть пустым")
+    @Size(max = 1024)
+    private String description;
+    private LocalDateTime created = LocalDateTime.now();
+
+    public ItemRequestDto(Integer userId, String description) {
+        this.userId = userId;
+        this.description = description;
+    }
 }
