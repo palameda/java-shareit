5d7406ba229b719cae20cd81344dae4e184dc1d4
palameda <stanislav.s.mazurenko@yandex.ru>
2024-03-18 20:39:11 +0300

    feat: добавить dto-класс ItemResponseDto (формат ответа)

--- src/main/java/ru/practicum/shareit/request/dto/ItemResponseDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemResponseDto.java
@@ -1,4 +1,29 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import ru.practicum.shareit.item.dto.ItemDto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * Data Transfer Object для сущности "Запрос на добавление вещи".
+ * Dto-класс ItemResponseDto содержит поля:
+ * <ul>
+ *     <li>id - идентификатор запроса, тип {@link Integer}</li>
+ *     <li>description - текстовое описание запроса, тип {@link String}</li>
+ *     <li>created - дата и время создания запроса, тип {@link LocalDateTime}</li>
+ *     <li>items - список запрошенных вещей, тип {@link ItemDto}</li>
+ * </ul>
+ */
+@Getter
+@Builder
+@AllArgsConstructor
 public class ItemResponseDto {
+    private Integer id;
+    private String description;
+    private LocalDateTime created;
+    private List<ItemDto> items;
 }
